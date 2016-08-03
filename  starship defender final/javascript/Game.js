7beb2baadc84bbb87ae31faaf72bf3a90a062d06
Shamusboy <james@crowtreesstudio.com>
2016-08-03 15:27:29 -0400

    Added Chapter 9 Code
    New enemy that shoots bullets

---  starship defender final/javascript/Game.js
+++  starship defender final/javascript/Game.js
@@ -13,14 +13,18 @@ window.onload = function(){
 var BasicGame = {};
 
 BasicGame.Game = function (game) {
-    this.PLAYER_SPEED = 200;
+    this.PLAYER_SPEED = 400;//200
     this.BULLET_DELAY = 100;
     this.playerNextBullet = 0;
     this.nextEnemy = 0;
+    this.ENEMY_BULLET_DELAY = 1000;
 };
 
 BasicGame.Game.prototype = {
     
+    /**************************************/
+    /*****         Init Method        *****/
+    /**************************************/
     init: function () {
         // init or initialization of the starting conditions of the game
         
@@ -39,27 +43,34 @@ BasicGame.Game.prototype = {
 
     },
     
+    /**************************************/
+    /*****        Preload Method      *****/
+    /**************************************/
     preload: function () {
 		// Preload the game images, sounds, etc first before starting the game
         this.load.spritesheet('player', 'assets/sprites/player.png', 64, 64);
         this.load.image('stars', 'assets/sprites/starBackground.png');
         this.load.image('bullet', 'assets/sprites/bullet.png');
         this.load.spritesheet('enemy', 'assets/sprites/enemy01.png', 32, 32);
+        this.load.spritesheet('enemyRed', 'assets/sprites/enemy02.png', 32, 32);
         this.load.spritesheet('explosion', 'assets/sprites/explosion.png', 32, 32);
         this.load.audio('laserShot', 'assets/sfx/Laser.Shoot.m4a',
-                        'assets/sfx/Laser.Shoot.m4a');
+                        'assets/sfx/Laser.Shoot.ogg');
         this.load.audio('explosion', 'assets/sfx/Explosion.m4a',
-                        'assets/sfx/Explosion.m4a');
+                        'assets/sfx/Explosion.ogg');
         this.load.audio('music', 'assets/music/Star.Command.John.Rawman.mp3');
 	},
     
-    
+    /**************************************/
+    /*****        Create Method       *****/
+    /**************************************/
     create: function () {
         // We instantiate or make our game objects here
         
         this.background = this.add.tileSprite(0, 0, 800, 600, 'stars');
         this.background.autoScroll(0, 100);
         
+        /*****      Player Ship Sprite       *****/
         this.player = this.add.sprite(this.world.centerX, 550, 'player');
         this.player.anchor.setTo(0.5, 0.5);
         this.player.animations.add('fly', [0, 1, 2], 30, true);
@@ -68,6 +79,7 @@ BasicGame.Game.prototype = {
         this.physics.arcade.enable(this.player);
         this.player.body.collideWorldBounds = true;
         
+        /*****      Player Bullet Group       *****/
         this.bulletGrp = this.add.group();
         this.bulletGrp.enableBody = true;
         this.bulletGrp.physicsBodyType = Phaser.Physics.ARCADE;
@@ -77,6 +89,7 @@ BasicGame.Game.prototype = {
         this.bulletGrp.setAll('anchor.x', 0.5);
         this.bulletGrp.setAll('anchor.y', 0.5);
         
+        /*****      Enemy Ship Group       *****/
         this.enemyGrp = this.add.group();
         this.enemyGrp.enableBody = true;
         this.enemyGrp.physicsBodyType = Phaser.Physics.ARCADE;
@@ -86,11 +99,23 @@ BasicGame.Game.prototype = {
         this.enemyGrp.setAll('anchor.x', 0.5);
         this.enemyGrp.setAll('anchor.y', 0.5);
         this.enemyGrp.setAll('value', 10, false, false, 0, true);
+        this.enemyGrp.setAll('nextBullet', 0, false, false, 0, true);
         this.enemyGrp.forEach(function(enemy){
             enemy.animations.add('fly', [0, 1, 2], 15, true);
             enemy.play('fly');
         }, this);
         
+        /*****      Enemy Bullet Group       *****/
+        this.enemyBulletGrp = this.add.group();
+        this.enemyBulletGrp.enableBody = true;
+        this.enemyBulletGrp.physicsBodyType = Phaser.Physics.ARCADE;
+        this.enemyBulletGrp.createMultiple(10, 'bullet');
+        this.enemyBulletGrp.setAll('checkWorldBounds', true);
+        this.enemyBulletGrp.setAll('outOfBoundsKill', true);
+        this.enemyBulletGrp.setAll('anchor.x', 0.5);
+        this.enemyBulletGrp.setAll('anchor.y', 0.5);
+        
+        /*****      Explosion Group       *****/
         this.explosionGrp = this.add.group();
         this.explosionGrp.createMultiple(10, 'explosion');
         this.explosionGrp.setAll('anchor.x', 0.5);
@@ -99,6 +124,7 @@ BasicGame.Game.prototype = {
             member.animations.add('bang');
         }, this);
         
+        /*****      GUI Player Lives    *****/
         this.livesTxt = this.add.text(35,
                                       24,
                                       "Lives:",
@@ -111,33 +137,39 @@ BasicGame.Game.prototype = {
             this.livesGrp.add(shipSprite);
         };
         
+        /*****      GUI Score    *****/
         this.scoreTxt = this.add.text(620,
                                       24,
                                       "Score: "+this.score,
                                       {font:'24px arial', fill:'#856f91'});
         
+        /*****      GUI Game Over    *****/
         this.gameOverTxt = this.add.text(this.world.centerX,
                                          this.world.centerY,
                                          "Game Over",
                                          {font:'48px arial', fill:'#856f91'});
         this.gameOverTxt.anchor.setTo(0.5);
         
+        /*****      GUI Play Again?    *****/
         this.playAgainTxt = this.add.text(this.world.centerX,
                                           this.world.centerY+this.gameOverTxt.height,
                                           "Play Again?\n Y / N",
                                           {font:'24px arial', fill:'#856f91', align:'center'});
         this.playAgainTxt.anchor.setTo(0.5);
         
+        /*****      GUI Group    *****/
         this.endGameTextGrp = this.add.group();
         this.endGameTextGrp.add(this.playAgainTxt);
         this.endGameTextGrp.add(this.gameOverTxt);
         this.endGameTextGrp.alpha = 0;
         
+        /*****      Audio Sprites       *****/
         this.shootSFX = this.add.audio('laserShot');
         this.explosionSFX = this.add.audio('explosion');
         this.music = this.add.audio('music');
-        this.music.loopFull(0.25);
+//        this.music.loopFull(0.25);
         
+        /*****      Player Keyboard Input      *****/
         this.arrowKeys = this.input.keyboard.createCursorKeys();
         
         this.keyInput = this.input.keyboard.addKeys({'shoot': Phaser.Keyboard.SPACEBAR,
@@ -146,6 +178,9 @@ BasicGame.Game.prototype = {
 
     },
 
+    /**************************************/
+    /*****        Update Method       *****/
+    /**************************************/
     update: function () {
         //  This is the game loop.        
         this.checkForInput();
@@ -161,10 +196,22 @@ BasicGame.Game.prototype = {
                                     null,
                                     this);
         
+        this.physics.arcade.overlap(this.player,
+                                    this.enemyBulletGrp,
+                                    this.playerHit,
+                                    null,
+                                    this);
+        
         this.spawnEnemy();
+        this.shootEnemyBullet();
 
     },
     
+    /**************************************/
+    /*****        Custom Methods      *****/
+    /**************************************/
+
+/*****      Playrt Keyboard Input Check     *****/
     checkForInput: function(){
         
         this.player.body.velocity.setTo(0, 0);
@@ -190,10 +237,12 @@ BasicGame.Game.prototype = {
         if(this.gameOver){
             if(this.keyInput.Yes.isDown){
                 this.state.restart();
+                this.music.stop();
             };
         };
     },
     
+/*****      Shoot Bullets Methods     *****/
     shootBullet: function(){
         if(this.playerNextBullet < this.time.now){
             var bullet = this.bulletGrp.getFirstDead();
@@ -209,14 +258,36 @@ BasicGame.Game.prototype = {
             this.playerNextBullet = this.time.now + this.BULLET_DELAY;
         };
     },
+    
+    shootEnemyBullet: function(){
+        if(this.player.alive){
+            this.enemyGrp.forEachAlive(function(enemy){
+                if(enemy.key === 'enemyRed'){
+                    if(enemy.nextBullet < this.time.now){
+                        var bullet = this.enemyBulletGrp.getFirstDead();
+                        if(bullet === null){
+                            return;
+                        };
+                        bullet.reset(enemy.x, enemy.y);
+                        this.physics.arcade.moveToObject(bullet, this.player, 500);
+                        this.shootSFX.play();
+                        enemy.nextBullet = this.time.now + this.ENEMY_BULLET_DELAY;
+                    };
+                };
+            }, this)
+        };
+    },
 
+/*****      Enemy and Player Hit Methods      *****/
     enemyHit: function(bullet, enemy){
         console.log("Enemy Hit");
         bullet.kill();
-        enemy.kill();
+        enemy.damage(1);
         this.blownUp(enemy);
-        this.score += enemy.value;
-        this.scoreTxt.text = "Score: "+this.score;
+        if(!enemy.alive){
+            this.score += enemy.value;
+            this.scoreTxt.text = "Score: "+this.score;
+        };
     },
     
     playerHit: function(player, enemy){
@@ -243,25 +314,43 @@ BasicGame.Game.prototype = {
         this.explosionSFX.play();
     },
     
+/*****      Add Enemies to the Game Screen     *****/
     spawnEnemy: function(){
         if(this.nextEnemy < this.time.now){
             var enemy = this.enemyGrp.getFirstDead();
             if(!enemy){
                 return;
             };
-            enemy.reset(this.rnd.integerInRange(20, 780), -16);
+            enemy.reset(this.rnd.integerInRange(20, 780), -10);
+            
+            var enemyShooter = Math.random();
+            
+            if(enemyShooter < 0.5){
+                enemy.loadTexture('enemyRed');
+                enemy.value = 20;
+                enemy.health = 2;
+            }else{
+                enemy.loadTexture('enemy');
+                enemy.value = 10;
+                enemy.health = 1;
+            };
+            
+            enemy.play('fly');
+            
             enemy.body.velocity.y = this.rnd.integerInRange(125, 200);
             
             this.nextEnemy = this.time.now + this.rnd.integerInRange(1000, 2000);
         };
     },
     
+/*****      End Game     *****/
     endGame: function(){
         this.add.tween(this.endGameTextGrp).to({alpha: 1},
                                                500,
                                                Phaser.Easing.Linear.None,
                                                true);
     },
+    
     
     render:function(){
         // post rendering effects
